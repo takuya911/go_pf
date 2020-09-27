20b312c75339ecc51acab0a41a13b82bd13eb7ab
takuya911 <takuya.engineer911@gmail.com>
2020-09-27 12:11:04 +0900

    とりあえず、ローカルでも動くdockerへ変更

--- services/graphql/main.go
+++ services/graphql/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -16,13 +17,13 @@ import (
 
 func main() {
 	// get env
-	// graphqlPort := os.Getenv("GRAPHQL_PORT")
-	// userServicePort := os.Getenv("USER_SERVICE_PORT")
+	graphqlPort := os.Getenv("GRAPHQL_PORT")
+	userServicePort := os.Getenv("USER_SERVICE_PORT")
 
 	ctx := context.Background()
 
 	// User gRPC connect
-	conn, err := grpc.DialContext(ctx, "user:50051" /*+userServicePort*/, grpc.WithInsecure())
+	conn, err := grpc.DialContext(ctx, "user:"+userServicePort, grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
@@ -35,6 +36,6 @@ func main() {
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", 80 /*graphqlPort*/)
-	log.Fatal(http.ListenAndServe(":80" /*+graphqlPort*/, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", graphqlPort)
+	log.Fatal(http.ListenAndServe(":"+graphqlPort, nil))
 }
