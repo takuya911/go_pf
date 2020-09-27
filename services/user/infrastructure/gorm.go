c470c347b0eaaf1640a231ed6aaf0106a44c55a4
takuya911 <takuya.engineer911@gmail.com>
2020-09-27 15:27:27 +0900

    test

--- services/user/infrastructure/gorm.go
+++ services/user/infrastructure/gorm.go
@@ -5,7 +5,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"google.golang.org/appengine"
 )
 
 // NewGormConnect function
@@ -16,15 +15,8 @@ func NewGormConnect() (*gorm.DB, error) {
 	pass := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
 
-	var connect string
-	if appengine.IsDevAppServer() {
-		instance := "go-pf-290702:asia-northeast1:go-pf-mysql"
-		connect = user + ":" + pass + "@unix(/cloudsql/" + instance + ")/" + dbName
-
-	} else {
-		protocol := "tcp(" + os.Getenv("INSTANCE_CONNECTION_NAME") + ")"
-		connect = user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-	}
+	instance := "go-pf-290702:asia-northeast1:go-pf-mysql"
+	connect := user + ":" + pass + "@unix(/cloudsql/" + instance + ")/" + dbName
 
 	db, err := gorm.Open("mysql", connect)
 	if err != nil {
