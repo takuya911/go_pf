7c1936d7fd59dc93eefe7bc4bfd84e880eb54814
takuya911 <takuya.engineer911@gmail.com>
2020-09-27 15:22:45 +0900

    ローカルかそれ以外かで接続を変えるように変更

--- services/user/go.mod
+++ services/user/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/jinzhu/gorm v1.9.16
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
+	google.golang.org/appengine v1.4.0
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
 )
--- services/user/infrastructure/gorm.go
+++ services/user/infrastructure/gorm.go
@@ -1,52 +1,37 @@
 package infrastructure
 
 import (
-	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-
 	"github.com/jinzhu/gorm"
+	"google.golang.org/appengine"
 )
 
 // NewGormConnect function
 func NewGormConnect() (*gorm.DB, error) {
-	// mysqlConf := &mysql.Config{
-	// 	User:                 os.Getenv("DB_USER"),
-	// 	Passwd:               os.Getenv("DB_PASS"),
-	// 	Net:                  "tcp",
-	// 	Addr:                 os.Getenv("INSTANCE_CONNECTION_NAME"),
-	// 	DBName:               os.Getenv("DB_NAME"),
-	// 	ParseTime:            true,
-	// 	Collation:            "utf8mb4_unicode_ci",
-	// 	Loc:                  time.Local,
-	// 	AllowNativePasswords: true,
-	// }
 
-	// db, err := gorm.Open("mysql", mysqlConf.FormatDSN())
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// if err := db.DB().Ping(); err != nil {
-	// 	return nil, err
-	// }
+	// get env
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
 
-	// return db, nil
-	var (
-		dbUser                 = os.Getenv("DB_USER")
-		dbPwd                  = os.Getenv("DB_PASS")
-		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
-		dbName                 = os.Getenv("DB_NAME")
-	)
+	var connect string
+	if appengine.IsDevAppServer() {
+		instance := "go-pf-290702:asia-northeast1:go-pf-mysql"
+		connect = user + ":" + pass + "@unix(/cloudsql/" + instance + ")/" + dbName
 
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-	if !isSet {
-		socketDir = "/cloudsql"
+	} else {
+		protocol := "tcp(" + os.Getenv("INSTANCE_CONNECTION_NAME") + ")"
+		connect = user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 	}
-	dbURI := fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
-	db, err := gorm.Open("mysql", dbURI)
+
+	db, err := gorm.Open("mysql", connect)
 	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %v", err)
+		return nil, err
+	}
+	if err := db.DB().Ping(); err != nil {
+		return nil, err
 	}
 
 	return db, nil
