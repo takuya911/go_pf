d898a27917d10e436fd44f5d1916f82a5443772c
takuya911 <takuya.engineer911@gmail.com>
2020-09-28 16:29:12 +0900

    user のbuildの情報も追加

--- services/graphql/build/push_dockerhub.sh
+++ services/graphql/build/push_dockerhub.sh
@@ -1,4 +1,3 @@
 # docker buildしてdocker hubにpushするだけ
-docker login -u takuyaatsugi9924 -p takuya9924
 docker build -t takuyaatsugi9924/go-pf_graphql:v1 -f Dockerfile ../
 docker push takuyaatsugi9924/go-pf_graphql:v1
