ecb562ac90c0e173a7927459043ad795b76da8ca
takuya911 <takuya.engineer911@gmail.com>
2020-09-28 10:46:52 +0900

    本番用

--- services/user/infrastructure/gorm.go
+++ services/user/infrastructure/gorm.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,14 +12,31 @@ import (
 func NewGormConnect() (*gorm.DB, error) {
 
 	// get env
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	dbName := os.Getenv("DB_NAME")
+	// user := os.Getenv("DB_USER")
+	// pass := os.Getenv("DB_PASS")
+	// dbName := os.Getenv("DB_NAME")
 
-	protocol := "tcp(" + os.Getenv("INSTANCE_CONNECTION_NAME") + ")"
-	connect := user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	// protocol := "tcp(" + os.Getenv("INSTANCE_CONNECTION_NAME") + ")"
+	// connect := user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	// db, err := gorm.Open("mysql", connect)
+	var (
+		dbUser                 = os.Getenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = os.Getenv("DB_PASS")                  // e.g. 'my-db-password'
+		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		dbName                 = os.Getenv("DB_NAME")                  // e.g. 'my-database'
+	)
 
-	db, err := gorm.Open("mysql", connect)
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
+
+	var dbURI string
+	dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
+
+	// dbPool is the pool of database connections.
+	db, err := gorm.Open("mysql", dbURI)
+
 	if err != nil {
 		return nil, err
 	}
