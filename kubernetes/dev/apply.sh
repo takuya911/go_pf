81abc63829c5f8927ce841d533cdb8f900a0fd33
takuya911 <takuya.engineer911@gmail.com>
2020-09-30 14:39:49 +0900

    修正

--- kubernetes/dev/apply.sh
+++ kubernetes/dev/apply.sh
@@ -1,10 +1,10 @@
-kubectl delete -f ./conf
+kubectl delete -f ./env
 kubectl delete -f ./mysql
 kubectl delete -f ./phpmyadmin
 kubectl delete -f ./user
 kubectl delete -f ./graphql
 
-kubectl apply -f ./conf
+kubectl apply -f ./env
 kubectl apply -f ./mysql
 kubectl apply -f ./phpmyadmin
 kubectl apply -f ./user
