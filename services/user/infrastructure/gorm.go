a254e040421bb640c0fe38f486710052ce470381
takuya911 <takuya.engineer911@gmail.com>
2020-10-08 13:13:30 +0900

    本番と開発で接続方法を切り替える

--- services/user/infrastructure/gorm.go
+++ services/user/infrastructure/gorm.go
@@ -10,12 +10,20 @@ import (
 // NewGormConnect function
 func NewGormConnect() (*gorm.DB, error) {
 
+	var connect string
+	env := os.Getenv("ENV")
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
 
-	protocol := "tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")"
-	connect := user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	if "prod" == env {
+		instanceConnectionName := os.Getenv("INSTANCE_CONNECTION_NAME")
+		connect = user + ":" + pass + "@unix(/cloudsql/" + instanceConnectionName + ")/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	} else {
+		dbHostName := os.Getenv("DB_HOST")
+		connect = user + ":" + pass + "@tcp(" + dbHostName + ":" + os.Getenv("DB_PORT") + ")/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	}
+
 	db, err := gorm.Open("mysql", connect)
 	if err != nil {
 		return nil, err
