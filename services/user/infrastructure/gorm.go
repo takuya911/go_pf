3392423d53b1e60a3da0399908c62fd4081d7476
takuya911 <takuya.engineer911@gmail.com>
2020-10-08 14:21:53 +0900

    接続方法変更

--- services/user/infrastructure/gorm.go
+++ services/user/infrastructure/gorm.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -19,16 +20,18 @@ func NewGormConnect() (*gorm.DB, error) {
 	if "dev" == env {
 		dbHostName := os.Getenv("DB_HOST")
 		connect = user + ":" + pass + "@tcp(" + dbHostName + ":" + os.Getenv("DB_PORT") + ")/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+		db, err := gorm.Open("mysql", connect)
+		if err != nil {
+			return nil, err
+		}
+		return db, nil
 	} else {
 		instanceConnectionName := os.Getenv("INSTANCE_CONNECTION_NAME")
-		connect = user + ":" + pass + "@unix(/cloudsql/" + instanceConnectionName + ")/" + dbName + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+		db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s", user, pass, instanceConnectionName, dbName))
+		if err != nil {
+			return nil, err
+		}
+		return db, nil
 	}
-
-	db, err := gorm.Open("mysql", connect)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 
 }
