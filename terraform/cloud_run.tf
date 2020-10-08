2a92060684de43601c6378ff54522c0cf0c1f4db
takuya911 <takuya.engineer911@gmail.com>
2020-10-08 12:44:05 +0900

    場所移動

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,3 +1,12 @@
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
 # Graphql Service
 resource "google_cloud_run_service" "graphql" {
   name     = "graphql"
@@ -38,19 +47,9 @@ resource "google_cloud_run_service" "graphql" {
   }
 }
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
 resource "google_cloud_run_service_iam_policy" "noauth" {
   location    = google_cloud_run_service.graphql.location
   project     = google_cloud_run_service.graphql.project
   service     = google_cloud_run_service.graphql.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
-
