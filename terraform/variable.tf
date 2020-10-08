bbc9d8564b814774f5d01650e3285dd0bd73fd98
takuya911 <takuya.engineer911@gmail.com>
2020-10-08 17:31:16 +0900

    port変更

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -15,9 +15,29 @@ variable "graphql_service_port" {
 }
 
 variable "user_service_name" {
-  default = "user"
+  default = "user-repftyfivq-an.a.run.app"
 }
 
 variable "user_service_port" {
-  default = "50051"
+  default = "443"
+}
+
+variable "user_image"{
+  default="gcr.io/golang-portfolio/go_pf/user@sha256:5bb6ce1e00cdf4711d05657e05d915f5a43e01ec7b9c4fb66da3b317db4027e9"
+}
+
+variable "db_name"{
+  default="go-pf-db"
+}
+
+variable "db_user"{
+  default="user"
+}
+
+variable "db_pass"{
+  default="password"
+}
+
+variable "db_connect_name"{
+  default="golang-portfolio:asia-northeast1:go-pf-instance"
 }
