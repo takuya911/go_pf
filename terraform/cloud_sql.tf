dd51075abf4779ad8c092d330ccff29f9659e656
takuya911 <takuya.engineer911@gmail.com>
2020-10-08 12:44:15 +0900

    cloud sql追加

---
+++ terraform/cloud_sql.tf
@@ -0,0 +1,12 @@
+resource "google_sql_database_instance" "instance" {
+  name   = "go-pf-instance"
+  region = "asia-northeast1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = "go-pf-db"
+  instance = google_sql_database_instance.instance.name
+}
