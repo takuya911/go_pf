1043f10b2d238757478d89541ebe602861b2278f
takuya911 <takuya.engineer911@gmail.com>
2020-10-13 10:48:07 +0900

    本番と分る

--- services/graphql/main.go
+++ services/graphql/main.go
@@ -8,11 +8,11 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-
 	"github.com/takuya911/go_pf/services/graphql/graph"
 	"github.com/takuya911/go_pf/services/graphql/graph/generated"
 	pb "github.com/takuya911/go_pf/services/graphql/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
@@ -20,15 +20,17 @@ func main() {
 	graphqlPort := os.Getenv("GRAPHQL_SERVICE_PORT")
 	userService := os.Getenv("USER_SERVICE_NAME")
 	userServicePort := os.Getenv("USER_SERVICE_PORT")
+	env := os.Getenv("ENV")
 
 	ctx := context.Background()
 
 	// User gRPC connect
-	conn, err := grpc.DialContext(ctx, userService+":"+userServicePort, grpc.WithInsecure())
+	conn, err := grpc.DialContext(ctx, userService+":"+userServicePort, grpcConnSecOption(env))
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
+
 	userC := pb.NewUserServiceClient(conn)
 
 	// Regist handler
@@ -39,4 +41,19 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", graphqlPort)
 	log.Fatal(http.ListenAndServe(":"+graphqlPort, nil))
+}
+
+// なんかconfに切り離せなかった
+func grpcConnSecOption(env string) grpc.DialOption {
+	var opt grpc.DialOption
+	if "dev" == env {
+		opt = grpc.WithInsecure()
+	} else {
+		creds, err := credentials.NewClientTLSFromFile("/etc/ssl/certs/ca-certificates.crt", "")
+		if err != nil {
+			log.Fatalf("failed to load credentials: %v", err)
+		}
+		opt = grpc.WithTransportCredentials(creds)
+	}
+	return opt
 }
